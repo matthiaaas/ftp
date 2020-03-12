e0781da7cefe4e33c910dd54db2c43f2a58e4292
matthiaaas <33065597+matthiaaas@users.noreply.github.com>
2020-03-12 17:53:03 +0100

    :bug: fixed application edit menu

--- public/electron.js
+++ public/electron.js
@@ -40,7 +40,7 @@ function createWindow() {
       ]
     }] : []),
     {
-      label: "Edit",
+      label: "Session",
       submenu: [
         {
           label: "New Folder",
@@ -82,6 +82,18 @@ function createWindow() {
             console.log("disconnecting")
           }
         }
+      ]
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        { role: "selectall" }
       ]
     },
     {
